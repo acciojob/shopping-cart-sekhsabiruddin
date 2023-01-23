f1105d3e5ca0b12e2cf85f82e552ffa33df5d109
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-01-23 18:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+
+
+let add =document.getElementById("add");
+
+add.addEventListener("click", function() {
+	/* Your CSS Code here. */
+let item = document.getElementById('item').value;
+let price =document.getElementById("price").value;
+ document.getElementById('total').innerText= item * price;
+});
